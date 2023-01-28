6712f4c7381e4337ac8e11f280df5aa73537c5b8
AsgerSuhr <asger31@gmail.com>
2023-01-28 18:15:34 +0100

    deleted build

--- build/CMakeFiles/picoRGBstrip_apa102_pio_h.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/picoRGBstrip_apa102_pio_h"
-  "apa102.pio.h"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/picoRGBstrip_apa102_pio_h.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
